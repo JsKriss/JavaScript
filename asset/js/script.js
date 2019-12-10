2312a4d8173b5e55ae2b9df2f4aee46d793563d9
JsKriss <57392242+JsKriss@users.noreply.github.com>
2019-12-10 12:49:18 +0100

    fonction isLeapYear

--- asset/js/script.js
+++ asset/js/script.js
@@ -16,17 +16,22 @@
 // document.write(message);
 
 
-let valeur = prompt('veuillez saisir un nombre ');
-let msg='';
-if (!isNaN(valeur)) {
-    if (valeur > 0) {
-        msg = 'positif';
-    } else if (valeur < 0) {
-        msg = 'negatif';
-    } else {
-        msg = 'nul';
-    }
-}
-else msg ='un nombre j\'ai dit';
-document.write(msg);
+// let valeur = prompt('veuillez saisir un nombre ');
+// let msg='';
+// if (!isNaN(valeur)) {
+//     if (valeur > 0) {
+//         msg = 'positif';
+//     } else if (valeur < 0) {
+//         msg = 'negatif';
+//     } else {
+//         msg = 'nul';
+//     }
+// }
+// else msg ='un nombre j\'ai dit';
+// document.write(msg);
+
+
+ document.getElementById('truc').innerText ='coucou';
+ document.getElementById('truc').style.color ='red';
+ document.getElementById('truc').style.backgroundColor ='black';
 
