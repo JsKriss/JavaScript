458915b8276f2fee38d214ada8f1459282eb7783
JsKriss <57392242+JsKriss@users.noreply.github.com>
2019-12-10 13:06:42 +0100

    fonction volume cylindre

--- asset/js/functions/isLeapYear.js
+++ asset/js/functions/isLeapYear.js
@@ -9,5 +9,3 @@ function isLeapYear(year){
     else{
         return false;
     }
-
-}
---
+++ asset/js/functions/volumecylincdre.js
@@ -0,0 +1,9 @@
+function volumecylindre (rayon,hauteur) {
+    if ((!isNaN(rayon) && !isNaN(hauteur)) &&(rayon >= 0 && hauteur >= 0)) {
+        return hauteur * Math.PI * Math.pow(rayon, 2);
+
+    }
+    else{
+        return false;
+    }
+}
