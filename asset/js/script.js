1418b278b809647fb5019a2ac312adaf65993c7b
JsKriss <57392242+JsKriss@users.noreply.github.com>
2019-12-10 10:40:51 +0100

    boucle for et condition avec modulo

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,4 +1,16 @@
-var message ='Je fais du js';
-message='<h1>'+message + '</h1>';
 
+
+let message ='<ul>';
+for (let i = -1722;i <= 2756;i++){
+    if (i % 9 ===0){
+        message+='<li>';
+        message+=i;
+        message +='</li>';
+    }
+}
+
+
+
+message += '</ul>';
+alert('aye!');
 document.write(message);
