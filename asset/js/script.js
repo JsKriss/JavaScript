9c130598842c4c67af1924ca4a5d09dddbc352b2
JsKriss <57392242+JsKriss@users.noreply.github.com>
2019-12-10 11:35:56 +0100

    correction avec la fonction isNaN()

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,16 +1,32 @@
+//
+//
+// let message ='<ul>';
+// for (let i = -1722;i <= 2756;i++){
+//     if (i % 9 ===0){
+//         message+='<li>';
+//         message+=i;
+//         message +='</li>';
+//     }
+// }
+//
+//
+//
+// message += '</ul>';
+// console.log('aye!');
+// document.write(message);
 
 
-let message ='<ul>';
-for (let i = -1722;i <= 2756;i++){
-    if (i % 9 ===0){
-        message+='<li>';
-        message+=i;
-        message +='</li>';
+let valeur = prompt('veuillez saisir un nombre ');
+let msg='';
+if (!isNaN(valeur)) {
+    if (valeur > 0) {
+        msg = 'positif';
+    } else if (valeur < 0) {
+        msg = 'negatif';
+    } else {
+        msg = 'nul';
     }
 }
+else msg ='un nombre j\'ai dit';
+document.write(msg);
 
-
-
-message += '</ul>';
-console.log('aye!');
-document.write(message);
